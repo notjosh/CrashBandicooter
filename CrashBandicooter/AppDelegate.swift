f3c88be5d4f3e907d902883d36b362354d6b1f26
Joshua May <notjosh@gmail.com>
2020-03-20 12:30:23 +0100

    Initial Commit

---
+++ CrashBandicooter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CrashBandicooter
+//
+//  Created by Joshua May on 20/3/20.
+//  Copyright © 2020 Joshua May. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CrashBandicooter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CrashBandicooter
+//
+//  Created by Joshua May on 20/3/20.
+//  Copyright © 2020 Joshua May. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
